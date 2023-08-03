d1cda57fabcb8ad63c5fc2445f387f0fa698b10a
jdvin <josh.daniel.vinson@gmail.com>
2023-08-04 08:51:37 +1000

    initial commit

---
+++ config.lua
@@ -0,0 +1,29 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.format_on_save = true
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { exe = "black", filetypes = { "python" } }
+}
+
+lvim.keys.normal_mode["|"] = ":vsplit<CR>"
+lvim.keys.normal_mode["-"] = ":split<CR>"
+lvim.keys.normal_mode["<C-Right>"] = ":bnext<CR>"
+lvim.keys.normal_mode["<C-Left>"] = ":bprev<CR>"
+
+
+lvim.plugins = {
+  "dccsillag/magma-nvim",
+  "ChristianChiarulli/swenv.nvim",
+  "stevearc/dressing.nvim",
+  "hrsh7th/nvim-cmp",
+  { "folke/neodev.nvim", opts = {} },
+}
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Python",
+  c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+}
