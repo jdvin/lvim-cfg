3d3bae35402efe78864500d40d69124f5155a070
jdvin <josh@simconverse.com>
2024-07-30 20:11:14 +1000

    made changes lol

--- config.lua
+++ config.lua
@@ -10,8 +10,6 @@ formatters.setup {
 
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
 lvim.keys.normal_mode["-"] = ":split<CR>"
-lvim.keys.normal_mode["<C-Right>"] = ":bnext<CR>"
-lvim.keys.normal_mode["<C-Left>"] = ":bprev<CR>"
 lvim.keys.term_mode["<C-`>"] = "<C-\\><C-n>"
 
 lvim.plugins = {
@@ -40,4 +38,14 @@ lvim.builtin.which_key.mappings["C"] = {
 
 lvim.builtin.which_key.mappings.s.t = {
   require('telescope').extensions.live_grep_args.live_grep_args, "Live grep args",
+}
+-- Show previewer when searching git files with default <leader>f
+lvim.builtin.which_key.mappings["f"] = {
+  require("lvim.core.telescope.custom-finders").find_project_files,
+  "Find File"
+}
+-- Show previewer when searching buffers with <leader>bf
+lvim.builtin.which_key.mappings.b.f = {
+  "<cmd>Telescope buffers<cr>",
+  "Find"
 }
